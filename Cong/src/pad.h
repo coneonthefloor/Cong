a248b39cb20d6e9ee03f9f859f7539e0216c1971
BrianDGLS <briandgls92@gmail.com>
2023-06-12 10:32:35 +0100

    feat: add paddle

---
+++ Cong/src/pad.h
@@ -0,0 +1,16 @@
+#include <raylib.h>
+
+typedef struct {
+	int UpButton;
+	int DownButton;
+} InputScheme;
+
+typedef struct {
+	InputScheme Scheme;
+	int Score;
+	float Speed;
+
+	Vector2 Position;
+	int Width;
+	int Height;
+} Pad;
